9e0dbb4d67676d813409e2a330d6409636b65aa6
Karl Jones <karljones@live.ie>
2022-11-10 14:50:51 +0000

    Added privacy policy

--- src/App.js
+++ src/App.js
@@ -1,70 +1,19 @@
 import React from 'react';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Privacy from './Pages/Privacy';
+import Home from './Pages/Home';
+
 function App() {
-
-  const yes = [
-    "Yes",
-    "YES!",
-    "¡SI!",
-    "No.. I mean, YES!",
-    "Halloween mode activated",
-    "🎃",
-    "Ya",
-    "Ja",
-    "Oui",
-    "はい"
-  ];
-  const yesSub = [
-    "Time to get spooky!",
-    "It's the most wonderful time of the year!",
-    "Which witch is which?",
-    "I'm a mouse.. Duh!",
-    "Light the sparklers!",
-  ];
-
-  const no = [
-    "No",
-    "Nah",
-    "Not yet",
-    "Nope",
-    "Nej",
-    "Non",
-    "Nein",
-    "No... But in Spanish",
-    "Nie",
-    "Não",
-    "Yes! Happy Not Opposite Day."
-  ];
-  const noSub = [
-    "Go back to work.",
-    "Put that costume back in your wardrobe.",
-    "It's too early for the spooky makeup you're wearing.",
-    "But you should definitely check back tomorrow, it might be then!",
-    "\"No capes!\" - Edna Mode, 2004",
-
-  ];
-
-  const [shareText, setShareText] = React.useState("It's not Halloween yet... Check back soon!");
-
-  const isItHalloween = () => {
-    const today = new Date();
-    const halloween = new Date(today.getFullYear, 10, 31);
-    return (today.getDate() === halloween.getDate()) && (today.getMonth() === halloween.getMonth());
-  }
-
   return (
-    <div className="App">
-      <h1 className="title">{isItHalloween() ? yes[Math.floor(Math.random() * yes.length)] : no[Math.floor(Math.random() * no.length)]}</h1>
-      <h3 className="subtitle">{isItHalloween() ? yesSub[Math.floor(Math.random() * yesSub.length)] : noSub[Math.floor(Math.random() * noSub.length)]}</h3>
-
-      {isItHalloween() && setShareText("It's Halloween! Time to get spooky!")}
-
-      {navigator.share &&
-        <button className="sharebtn" aria-label="share" onClick={() => { navigator.share({ title: document.title, text: shareText, url: window.location.href }) }}>
-          Share
-        </button>
-      }
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Privacy.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Privacy = () => {
+    return (
+        <div style={{ width: '90%', textAlign: 'start', marginLeft: 'auto', marginRight: 'auto' }}>
+            <h1 style={{ textAlign: 'center' }}>Privacy Policy</h1>
+            <p>This website is operated by <a href="https://karljones.me">Karl Jones</a> and whose registered aaddress is G41 2EG, United Kingdom. ("We") are committed to protecting and preserving the privacy of our visitors when visiting out site.</p>
+            <p>This policy sets out how we process any personal data we collect from you or that you provide to us through our website. We confirm that we will keep your information secure and that we will comply fully with all applicable UK Data Protection legislation and regulations. Please read the following carefully to understand what happens to personal data that you choose to provide to us, or that we collect from you when you visit this site. By visiting https://isithalloween.today (our website) you are accepting and consenting to the practices described in this policy.</p>
+            <h3 style={{ textAlign: 'center' }}>Information we automatically collect</h3>
+            <p>"Is it Halloween?" does not collect information on you, however, we do use Google to serve ads:</p>
+            <ul>
+                <li>Third party vendors, including Google, use cookies to serve ads based on a user's previous visits to the website, or other websites</li>
+                <li>Google's use of advertising cookies enables it and its partners to serve ads to the users of the site.</li>
+                <li>You may opt out of personalised advertising by visiting your Ads Settings through your Google account</li>
+                <li>You may also opt out of personalised ads by visiting <a href="https://www.aboutads.info">www.aboutads.info</a></li>
+            </ul>
+            <h3 style={{ textAlign: 'center' }}>Third party links</h3>
+            <p>Our site may, from time to time, contain links to and from the third party websites. If you follow a link to any of these websites, please note that these websites have their own privacy policies and that we do not accept any responsibility or liability for these policies. Please check these policies before you submit any personal data to these websites.</p>
+            <h3 style={{ textAlign: 'center' }}>TLDR;</h3>
+            <p>I, developer, have no interest in your data but do use ads in order to recoup the costs of hosting and the domain. I would encourage everyone to opt out of personalised ads, regardless of usage of this website.</p>
+        </div>
+    )
+}
+
+export default Privacy;
